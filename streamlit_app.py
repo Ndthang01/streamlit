135cc6c66ed8a2eba5d2c8f878b45088b8bdeb03
Ndthang01 <67347297+Ndthang01@users.noreply.github.com>
2022-01-06 17:55:40 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ def main():
     # col1, col2 = st.columns([6, 4])
     # with col2:
         # st.image(f"{resources_dir}/wordcloud.png", width=700)
-    col1 = st.columns([6, 4])
+    col1 = st.columns([6])
     with col1:
         model_name = st.selectbox("Choose your model", index=0, options=list(model_dict.keys()))
 
